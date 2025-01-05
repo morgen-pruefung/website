6a21061d8d7cea4ec179d9bdb06698c37248614b
Oliver <oschi1303@gmail.com>
2025-01-05 18:22:36 +0100

    Add newsletter page

---
+++ src/pages/newsletter/index.vue
@@ -0,0 +1,81 @@
+<script lang="ts" setup>
+
+import {newsletterSubscribe} from "@/api/newsletter/newsletter";
+
+const email = ref<string>('');
+const emailRules = [
+  (v: string) => !!v || 'E-Mail ist erforderlich',
+  (v: string) => /.+@.+\..+/.test(v) || 'E-Mail muss gültig sein',
+];
+const valid = computed(() => {
+  return emailRules.every(rule => rule(email.value) === true);
+});
+
+const registered = ref<boolean>(false);
+
+async function register() {
+  await newsletterSubscribe({email: email.value})
+
+  window.alert('Erfolgreich für den Newsletter registriert');
+  registered.value = true;
+}
+
+</script>
+
+<template>
+  <v-container class="w-50">
+    <v-row>
+      <v-col>
+        <h1>Newsletter</h1>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <p>Registiere dich jetzt bei unseren Newsletter, um Neuigkeiten nicht zu verpassen. Es erwarten dich Updates rund um Morgen-Pruefung.de, sowie neue Informationen über die Ausbildungsberufe und deren Themen.</p>
+      </v-col>
+    </v-row>
+
+    <v-row v-if="registered">
+      <v-col>
+        <v-alert
+          class="my-8"
+          color="success"
+          icon="mdi-check"
+        >
+          Erfolgreich für den Newsletter registriert
+        </v-alert>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-text-field
+          v-model="email"
+          :disabled="registered"
+          :rules="emailRules"
+          class="w-50"
+          color="primary"
+          label="Email"
+          placeholder="z. B. beispiel@gmail.com"
+          variant="outlined"/>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-btn
+          :disabled="registered || !valid"
+          color="primary"
+          @click="register"
+        >
+          Registrieren
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -22,6 +22,7 @@ declare module 'vue-router/auto-routes' {
     '/blog/': RouteRecordInfo<'/blog/', '/blog', Record<never, never>, Record<never, never>>,
     '/blog/[id]': RouteRecordInfo<'/blog/[id]', '/blog/:id', { id: ParamValue<true> }, { id: ParamValue<false> }>,
     '/home': RouteRecordInfo<'/home', '/home', Record<never, never>, Record<never, never>>,
+    '/newsletter/': RouteRecordInfo<'/newsletter/', '/newsletter', Record<never, never>, Record<never, never>>,
     '/pruefungen/': RouteRecordInfo<'/pruefungen/', '/pruefungen', Record<never, never>, Record<never, never>>,
     '/pruefungen/ap1/': RouteRecordInfo<'/pruefungen/ap1/', '/pruefungen/ap1', Record<never, never>, Record<never, never>>,
     '/themen/': RouteRecordInfo<'/themen/', '/themen', Record<never, never>, Record<never, never>>,
